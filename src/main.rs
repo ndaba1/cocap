a0f4af50ace73913f0ed3298c790e2947ff6adbc
Victor Ndaba <mwangindaba@gmail.com>
2022-03-14 23:12:52 +0300

    feat: refactored using cmder, added core commands

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,44 @@
-use clap::ArgMatches;
-use cocap::{self, Program};
+use cmder::Program;
 
 fn main() {
-    let program = Program::new();
+    let mut program = Program::new();
 
-    let cfg = program.get_matches();
+    program
+        .version("0.1.0")
+        .description("A wrapper around git to increase developer productivity");
 
-    dbg!(&cfg);
+    program
+        .add_cmd()
+        .command("stats [app-name]")
+        .alias("status")
+        .describe("Print out statistics about your latest commits")
+        .action(|vals, opts| {
+            dbg!(vals);
+            dbg!(opts);
+        })
+        .build(&mut program);
 
-    match cfg.subcommand() {
-        Some(cmd) => execute_cmd(cmd.0, cmd.1),
-        None => {
-            println!("Your command wasn't recognized")
-        }
-    }
-}
+    program
+        .add_cmd()
+        .command("add <dir-name>")
+        .alias("track")
+        .describe("Start tracking a new directory with git and cocap")
+        .action(|vals, opts| {
+            dbg!(vals);
+            dbg!(opts);
+        })
+        .build(&mut program);
 
-fn execute_cmd(name: &str, vals: &ArgMatches) {
-    match name {
-        "stats" => cocap::cmd::stats(vals),
-        _ => {}
-    }
+    program
+        .add_cmd()
+        .command("activate <dir-name>")
+        .alias("act")
+        .describe("Change directory into the project with the given name")
+        .action(|vals, opts| {
+            dbg!(vals);
+            dbg!(opts);
+        })
+        .build(&mut program);
+
+    program.parse();
 }
