e27e2434e5a2ebb6618a0c4da2d3b05044a8db36
Victor Ndaba <mwangindaba@gmail.com>
2022-03-20 23:06:02 +0300

    📦 feat: started work on the settings module

--- src/lib/core/settings.rs
+++ src/lib/core/settings.rs
@@ -1,7 +1,27 @@
 use std::env::vars;
+use std::path::Path;
 
 pub fn check_config() {
-    let vals = vars();
+    let full_path = if cfg!(windows) {
+        let prefix = get_env_value("HOMEDRIVE").unwrap();
+        let path = get_env_value("HOMEPATH").unwrap();
 
-    dbg!(vals);
+        Path::new(&prefix).join(path)
+    } else {
+        let path = get_env_value("HOME").unwrap();
+
+        Path::new(&path).to_path_buf()
+    };
+
+    dbg!(full_path);
+}
+
+fn get_env_value(val: &str) -> Option<String> {
+    let vals: Vec<_> = vars().collect();
+    let result = vals.iter().find(|v| v.0 == val);
+
+    match result {
+        None => None,
+        Some(v) => Some(v.1.clone()),
+    }
 }
