03c26bdd4afcc21dfcb10da1e1b986af4855c14b
Victor Ndaba <mwangindaba@gmail.com>
2022-06-05 18:23:56 +0300

    📦 feat: added new commands

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use cmder::{Designation, Event, Formatter, Program};
+use cmder::{Designation, Event, Formatter, Program, Setting};
 use figlet_rs::FIGfont;
 
 use cocap::cmd;
@@ -13,82 +13,94 @@ fn main() {
         .description("A wrapper around git to increase developer productivity");
 
     program
-        .command("stats")
+        .subcommand("stats")
         .alias("status")
         .description("Print out statistics about your latest commits")
-        .option(
-            "-c --count <count-size>",
-            "Specify how far the stats should show",
-        )
-        .option(
-            "-d --dir <dir-name>",
-            "Print out stats for a specific directory",
-        )
-        .action(cmd::stats)
-        .build(&mut program);
+        .option("-d --dir <name>", "Show stats for a specific directory")
+        .option("--count <number>", "The number of stats to show")
+        .action(cmd::stats);
 
     program
-        .command("add <dir-name>")
+        .subcommand("add")
+        .argument("<dir-name>", "The directory to add")
         .alias("track")
         .description("Start tracking a new directory with git and cocap")
         .option(
-            "-r --root",
-            "Track all git initialized directories in a given root dir",
+            "-r --recursive <depth>",
+            "Recursively add all subdirectories at provided depth",
         )
-        .action(cmd::add)
-        .build(&mut program);
+        .action(cmd::add);
 
     program
-        .command("activate <dir-name>")
+        .subcommand("activate")
+        .argument("<dir-name>", "The directory to activate")
         .alias("act")
         .description("Change directory into the project with the given name")
-        .action(|vals, opts| {
-            dbg!(vals);
-            dbg!(opts);
-        })
-        .build(&mut program);
+        .action(cmd::activate);
 
     program
-        .command("ui")
-        .alias("serve")
+        .subcommand("ui")
+        .alias("server")
         .description("Start the UI server on a default or provided port")
-        .option("-p --port", "Specify a custom port to start the server on")
-        .action(|vals, opts| {
-            dbg!(vals);
-            dbg!(opts);
-        })
-        .build(&mut program);
+        .option(
+            "-p --port <port-number>",
+            "Specify a custom port to start the server on",
+        )
+        .action(|m| {
+            dbg!(m);
+        });
 
     program
-        .command("push [dir-name]")
+        .subcommand("push")
         .alias("p")
         .description("Push changes to their respective remote repositories")
         .option("-a --all", "Push all changes in all tracked directories")
-        .action(|_v, _o| {})
-        .build(&mut program);
+        .action(|m| {
+            dbg!(m);
+        });
 
     program
-        .command("config")
+        .subcommand("config")
         .alias("cfg")
         .description("Set the configurations for cocap")
         .option("-g --global", "Configure global cocap options")
-        .action(|_v, _o| {})
-        .build(&mut program);
+        .action(|m| {
+            dbg!(m);
+        });
 
-    program.on(Event::OutputVersion, |p, v| {
+    program
+        .subcommand("remove")
+        .alias("rm")
+        .argument("<dir-name>", "The name of the directory/project")
+        .description("Stop tracking the given directory name")
+        .action(cmd::remove);
+
+    program
+        .subcommand("clean")
+        .alias("reset")
+        .description("Remove all tracked projects and reset to default config")
+        .action(cmd::clean);
+
+    program.on(Event::OutputVersion, |cfg| {
         let standard_font = FIGfont::standand().unwrap();
         let figure = standard_font.convert("cocap").unwrap();
+        let prog = cfg.get_program();
 
-        let mut fmtr = Formatter::default();
+        let mut fmtr = Formatter::new(prog.get_theme().to_owned());
         fmtr.add(Designation::Keyword, &figure.to_string());
-        fmtr.add(Designation::Headline, &format!("cocap v{}\n", &v));
+        fmtr.add(
+            Designation::Headline,
+            &format!("cocap v{}\n", prog.get_version()),
+        );
         fmtr.add(
             Designation::Description,
-            &format!("{}\n", p.get_description()),
+            &format!("{}\n", prog.get_description()),
         );
 
         fmtr.print();
     });
+
+    program.set(Setting::HideCommandAliases(false));
 
     program.parse();
 }
